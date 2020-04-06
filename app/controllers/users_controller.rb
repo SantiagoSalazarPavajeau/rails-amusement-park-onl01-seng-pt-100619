b7d82a58107b6b3165dacc2ff10094e7e42738f6
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-06 15:38:36 +0200

    pass first signup test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,31 @@ class UsersController < ApplicationController
     end
 
     def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def index
+        @users = User.all
+    end
+
+    def edit
+        @user = User.find(params[:id])
     end
 
     private
+
+    def user_params
+        params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password)
+    end
 
   
 end
